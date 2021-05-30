f4efb828984a9c95c4487ccb94bcffe1a2d14c02
reecewang <928082159@qq.com>
2021-05-30 20:58:03 +0800

    增加报价、降价弹窗

--- src/components/CarInfoCard.js
+++ src/components/CarInfoCard.js
@@ -1,4 +1,5 @@
-import {Card, Space , Button} from 'antd';
+import {Card, Space, Button, Modal, InputNumber, Input, Form} from 'antd';
+import React, { useState } from 'react';
 
 const gridStyle = {
     width: '25%',
@@ -6,13 +7,37 @@ const gridStyle = {
 };
 
 const CarCard = ({name, price, brand, mileage, regDate}) => {
+
+    const [isModalVisiblePrice, setIsModalVisiblePrice] = useState(false);
+    const showModalPrice = () => {
+        setIsModalVisiblePrice(true);
+    };
+    const handleOkPrice = () => {
+
+    };
+    const handleCancelPrice = () => {
+        setIsModalVisiblePrice(false);
+    };
+
+    const [isModalVisibleBuy, setIsModalVisibleBuy] = useState(false);
+    const showModalBuy = () => {
+        setIsModalVisibleBuy(true);
+    };
+    const handleOkBuy = () => {
+        setIsModalVisibleBuy(false);
+    };
+    const handleCancelBuy = () => {
+        setIsModalVisibleBuy(false);
+    };
+
+
     return (
         <div className="site-card-border-less-wrapper">
             <Card style={{width: 600}} title={name}>
                 <Space direction="vertical" size="middle">
                     <Card type="inner" title={"车主报价：" + Math.round(price * 100) / 100 + "万"}
                         // eslint-disable-next-line
-                          extra={<a>降价提醒</a>}>
+                          extra={<a onClick={showModalPrice}>降价提醒 </a>}>
                         服务费：{Math.round(price * 100) / 10000} 万元 （预估）(最终以成交价格的1%为准）
                     </Card>
                     <Card type="inner">
@@ -25,9 +50,27 @@ const CarCard = ({name, price, brand, mileage, regDate}) => {
                     <Card type="inner" title="售后保障">
                         30天无忧退车 30天全面保修
                     </Card>
-                    <Button type="primary" block>立即报价</Button>
+                    <Button type="primary" block onClick={showModalBuy} >立即报价</Button>
                 </Space>
             </Card>
+
+            <>
+                <Modal title="降价提醒" visible={isModalVisiblePrice} onOk={handleOkPrice} onCancel={handleCancelPrice}>
+                    <p>卖家报价：{Math.round(price * 100) / 100} 万 建议入手价格：{Math.round(price * 100) / 100 - 1} - {Math.round(price * 100) / 100} 万</p>
+                    <p><Input  suffix="万" placeholder="降到多少通知我" /></p>
+                    <p><Input placeholder="请输入您的手机号" /></p>
+                </Modal>
+            </>
+
+            <>
+                <Modal title="请输入您的预期价格" visible={isModalVisibleBuy} onOk={handleOkBuy} onCancel={handleCancelBuy}>
+                    <p>卖家报价：{Math.round(price * 100) / 100} 万 </p>
+                    <p><Input  suffix="万" placeholder="期望价格" /></p>
+                    <p><Input placeholder="请输入您的手机号" /></p>
+                </Modal>
+            </>
+
+
         </div>
     )
 }
