5273ffea57a7ad09c392d3db872320c634fc2b35
qiaozp <chivalry.pp@gmail.com>
2021-09-09 16:07:43 +0800

    fix deploy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,6 +4,7 @@ module.exports = function (app) {
     app.use(
         proxy('/api', {
             target: 'http:/roderick.ltd/api',
+            // target: 'http://124.70.3.195:4567',
             // target: 'http://localhost:4567',
             changeOrigin: true,
             pathRewrite: {
