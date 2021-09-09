6242a83a01dedeb78570a4943a47873ad5f080bf
qiaozp <chivalry.pp@gmail.com>
2021-09-09 10:44:17 +0800

    move to domain

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware')
 module.exports = function (app) {
     app.use(
         proxy('/api', {
-            target: 'http://124.70.3.195:4567',
+            target: 'http:/roderick.ltd/api',
             // target: 'http://localhost:4567',
             changeOrigin: true,
             pathRewrite: {
